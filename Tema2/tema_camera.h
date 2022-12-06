96e70eb1a6a4d59c85eda21fad7619735aa1d3bb
ionutpasat <74329356+ionutpasat@users.noreply.github.com>
2022-12-06 03:11:48 +0200

    Add files via upload

---
+++ Tema2/tema_camera.h
@@ -0,0 +1,154 @@
+#pragma once
+
+#include "utils/glm_utils.h"
+#include "utils/math_utils.h"
+
+
+namespace implemented
+{
+    class HwCamera
+    {
+     public:
+        HwCamera()
+        {
+            position    = glm::vec3(0, 2, 5);
+            forward     = glm::vec3(0, 0, -1);
+            up          = glm::vec3(0, 1, 0);
+            right       = glm::vec3(1, 0, 0);
+            distanceToTarget = 2;
+        }
+
+        HwCamera(const glm::vec3 &position, const glm::vec3 &center, const glm::vec3 &up)
+        {
+            Set(position, center, up);
+        }
+
+        ~HwCamera()
+        { }
+
+        // Update HwCamera
+        void Set(const glm::vec3 &position, const glm::vec3 &center, const glm::vec3 &up)
+        {
+            this->position = position;
+            forward     = glm::normalize(center - position);
+            right       = glm::cross(forward, up);
+            this->up    = glm::cross(right, forward);
+        }
+
+        void MoveForward(float distance)
+        {
+            // Translates the HwCamera using the `dir` vector computed from
+            // `forward`. Movement will always keep the HwCamera at the same
+            // height. For example, if you rotate your head up/down, and then
+            // walk forward, then you will still keep the same relative
+            // distance (height) to the ground!
+            glm::vec3 dir = glm::normalize(glm::vec3(forward.x, 0, forward.z));
+            position += dir * distance;
+        }
+
+        void TranslateForward(float distance)
+        {
+            // TODO(student): Translate the HwCamera using the `forward` vector.
+            // What's the difference between `TranslateForward()` and
+            // `MoveForward()`?
+            position += glm::vec3(forward.x, 0 , forward.z) * distance;
+        }
+
+        void TranslateUpward(float distance)
+        {
+            // TODO(student): Translate the HwCamera using the `up` vector.
+            position += glm::vec3(0, 1, 0) * distance;
+        }
+
+        void TranslateRight(float distance)
+        {
+            // TODO(student): See instructions below. Read the entire thing!
+            // You need to translate the HwCamera using the `right` vector.
+            // Usually, however, translation using HwCamera's `right` vector
+            // is not very useful, because if the HwCamera is rotated around the
+            // `forward` vector, then the translation on the `right` direction
+            // will have an undesired effect, more precisely, the HwCamera will
+            // get closer or farther from the ground. The solution is to
+            // actually use the projected `right` vector (projected onto the
+            // ground plane), which makes more sense because we will keep the
+            // same distance from the ground plane.
+            position += glm::vec3(right.x, 0, right.z) * distance;
+        }
+
+        void RotateFirstPerson_OX(float angle)
+        {
+            // TODO(student): Compute the new `forward` and `up` vectors.
+            // Don't forget to normalize the vectors! Use `glm::rotate()`.
+            glm::mat4 rotation = glm::rotate(glm::mat4(1.f), angle, right);
+            forward = rotation * glm::vec4(forward, 1);
+            up = glm::cross(right, forward);
+        }
+
+        void RotateFirstPerson_OY(float angle)
+        {
+            // TODO(student): Compute the new `forward`, `up` and `right`
+            // vectors. Use `glm::rotate()`. Don't forget to normalize the
+            // vectors!
+            glm::mat4 rotation = glm::rotate(glm::mat4(1.f), angle, glm::vec3(0, 1, 0));
+            forward = rotation * glm::vec4(forward, 1);
+            up = rotation * glm::vec4(up, 1);
+            right = glm::cross(forward, up);
+        }
+
+        void RotateFirstPerson_OZ(float angle)
+        {
+            // TODO(student): Compute the new `right` and `up`. This time,
+            // `forward` stays the same. Use `glm::rotate()`. Don't forget
+            // to normalize the vectors!
+            glm::mat4 rotation = glm::rotate(glm::mat4(1.f), angle, forward);
+            right = rotation * glm::vec4(right, 1);
+
+            up = glm::cross(right, forward);
+        }
+
+        void RotateThirdPerson_OX(float angle)
+        {
+            // TODO(student): Rotate the HwCamera in third-person mode around
+            // the OX axis. Use `distanceToTarget` as translation distance.
+            TranslateForward(distanceToTarget);
+            RotateFirstPerson_OX(angle);
+            TranslateForward(-distanceToTarget);
+        }
+
+        void RotateThirdPerson_OY(float angle)
+        {
+            // TODO(student): Rotate the HwCamera in third-person mode around
+            // the OY axis.
+            TranslateForward(distanceToTarget);
+            RotateFirstPerson_OY(angle);
+            TranslateForward(-distanceToTarget);
+        }
+
+        void RotateThirdPerson_OZ(float angle)
+        {
+            // TODO(student): Rotate the HwCamera in third-person mode around
+            // the OZ axis.
+            TranslateForward(distanceToTarget);
+            RotateFirstPerson_OZ(angle);
+            TranslateForward(-distanceToTarget);
+        }
+
+        glm::mat4 GetViewMatrix()
+        {
+            // Returns the view matrix
+            return glm::lookAt(position, position + forward, up);
+        }
+
+        glm::vec3 GetTargetPosition()
+        {
+            return position + forward * distanceToTarget;
+        }
+
+     public:
+        float distanceToTarget;
+        glm::vec3 position;
+        glm::vec3 forward;
+        glm::vec3 right;
+        glm::vec3 up;
+    };
+}   // namespace implemented
